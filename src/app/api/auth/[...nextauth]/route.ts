c4c7def375397995e29e34f4a6307eea890dd028
TaiFilipelli <taielfilipelli@gmail.com>
2025-02-24 13:37:42 -0300

    still trying to fix issue with the deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import { authOptions } from "@/lib/authOptions";
 
-export const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-        if (session.user) {
-            session.user.id = token.id as string;
-    }
-      return session;
-    },
-    async redirect({ baseUrl }) {
-      return `${baseUrl}/login/result`;
-    },
-  },
-});
-
-export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions);
+export {handler as GET, handler as POST};
