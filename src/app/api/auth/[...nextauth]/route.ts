9681512d032e90ed3dd50e297a5ce1e3f469395a
TaiFilipelli <taielfilipelli@gmail.com>
2025-02-24 13:29:42 -0300

    trying to solve the deploy issue

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,33 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { NextAuthOptions } from "next-auth";
 
-
-export const authOptions:NextAuthOptions = ({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-        //El (!) al final fuerza a Typescript a que tome el valor entrante , y que no será undefined
-    ],
-    session:{
-        strategy:"jwt",
+export const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
     },
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                token.id = user.id;
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            if(session.user){
-
-                session.user.id = token.id as string;
-            }
-            return session;
-        },
-        async redirect({ baseUrl}){
-            return `${baseUrl}/login/result`;
-        }
+    async session({ session, token }) {
+        if (session.user) {
+            session.user.id = token.id as string;
+    }
+      return session;
     },
+    async redirect({ baseUrl }) {
+      return `${baseUrl}/login/result`;
+    },
+  },
 });
 
-// const handler = NextAuth(authOptions);
-
-export { NextAuth as GET, NextAuth as POST };
+export { handler as GET, handler as POST };
