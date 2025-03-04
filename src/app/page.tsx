c70766cfe6507d9c8bbdd67cb8366d8d42616c6e
TaiFilipelli <taielfilipelli@gmail.com>
2025-03-04 14:46:15 -0300

    new loader in main page for better ux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import api from "@/product/api";
 import { useEffect, useState } from "react";
 import { useCartStore } from "@/store/useCartStore";
 import ProductCard from "@/components/ProductCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
 
@@ -29,17 +31,23 @@ export default function Home() {
 
   }, []);
 
-
   return (
     <main className="m-10 text-center">
       <h2 className="text-2xl font-bold m-5 pb-12">Tuneá a la maleducada como te parezca. Lo necesitas? Lo tenemos.</h2> 
       <section className="bg-white rounded-2xl items-center justify-center flex flex-col p-10 mb-10">
         <h1 className="font-bold text-5xl text-black mb-10">Productos</h1>
-        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} addToCart={addToCart} />
-          ))}
-        </section>
+        { products.length>0 ? 
+          <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} addToCart={addToCart} />
+            ))}
+          </section>
+          :
+          <section className="flex flex-col gap-4 m-10">
+            <h1 className="text-xl font-semibold text-black mb-4">Preparando productos...</h1>
+            <FontAwesomeIcon icon={faCircleNotch} spin size="2xl" />
+          </section>
+        }
       </section>
     </main>
   );
