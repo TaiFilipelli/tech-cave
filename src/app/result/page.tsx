8a9fa1902bce1e92b03ba7713d38c90fcfb2e5ac
TaiFilipelli <taielfilipelli@gmail.com>
2025-03-10 15:03:41 -0300

    new result page to future payment redirection

---
+++ src/app/result/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PaymentResult = () => {
+  return (
+    <section className='p-10 bg-gray-200 rounded-lg shadow-md'>
+      <h1 className='text-2xl font-bold'>Pago hecho/no hecho/problemático</h1>
+    </section>
+  )
+}
+
+export default PaymentResult
