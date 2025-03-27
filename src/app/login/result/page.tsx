9d19952b67f18be0f44863b91e157fba4ef6ed51
TaiFilipelli <taielfilipelli@gmail.com>
2025-03-27 18:15:42 -0300

    more tests, man

--- src/app/login/result/page.tsx
+++ src/app/login/result/page.tsx
@@ -68,7 +68,7 @@ const LoginResult = () => {
 
         console.log('Datos de usuario a añadir:', user, '; entrariamos a la ruta /api/users');
 
-        const addUserResponse = await fetch("/api/users", {
+        const addUserResponse = await fetch("../api/users", {
           method: 'POST',
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(user),
