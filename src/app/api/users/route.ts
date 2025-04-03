e54763d40a48d4f45c53a13f236b1ed0f9e543da
TaiFilipelli <taielfilipelli@gmail.com>
2025-04-03 10:39:12 -0300

    fully functional middleware and new GET endpoint to search a user's data

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,33 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
+
+export async function GET(req:Request){
+    try {
+
+        const {searchParams} = new URL(req.url);
+        const email = searchParams.get('email');
+
+        if(!email){
+            return NextResponse.json({ success: false, error: "Email is required" }, { status: 400 });
+        }
+
+        const client = await clientPromise;
+        const db = client.db(process.env.MONGODB_DB);
+        const usersCollection = db.collection("users");
+
+        const user = await usersCollection.findOne({ email });
+
+        if(!user){
+            return NextResponse.json({ success: false, error: "No user found" }, { status: 404 });
+        }
+
+        return NextResponse.json({success:true, user});
+    }catch(error){
+        return NextResponse.json({ success: false, error: error }, { status: 500 });
+    }
+}
+
 export async function POST(req:Request){
     try {
         console.log('Entrando al POST');
---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req:NextRequest){
+
+    const { pathname } = req.nextUrl;
+    
+    
+    if(pathname.startsWith('/dashboard')){
+        const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+        const email = token?.email;
+
+        if(email){
+            return NextResponse.redirect(new URL('/404', req.url));
+        }
+
+        try{
+            const response = await fetch(`/api/users?email=${encodeURIComponent(email!)}`,{
+                method:'GET',
+            });
+            const data = await response.json();
+
+            if(!data.success || !data.user){
+                return NextResponse.redirect(new URL('/404', req.url));
+            }
+
+            if(!data.user.isAdmin){
+                return NextResponse.redirect(new URL('/404', req.url));
+            }
+            
+            return NextResponse.next();
+
+        }catch(error){
+            return NextResponse.json({success: false, error: error}, {status:500});
+        }
+
+    }
+}
