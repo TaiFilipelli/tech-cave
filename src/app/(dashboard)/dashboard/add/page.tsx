cb0fba2f59a9b53149d284913358dca58bcd8ce0
TaiFilipelli <taielfilipelli@gmail.com>
2025-04-07 10:31:31 -0300

    made the test. This is working perfectly fine (at least on local)

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -1,10 +1,17 @@
+'use client'
 import React from 'react'
+import { useProducts } from '@/product/provider'
 
 const AddPage = () => {
+
+  const products = useProducts()
+
+
   return (
     <section className='flex flex-col items-center justify-center p-20'>
       <h1 className='font-bold text-3xl'>Agregar producto</h1>
-      
+      <h2 className='font-semibold text-xl my-5'>ADVERTENCIA: El texto a continuación únicamente sirve para chequear si funca. Parece que si.</h2>
+      <p>{JSON.stringify(products)}</p>
     </section>
   )
 }
