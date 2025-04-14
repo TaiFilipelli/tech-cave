b5c7feeb77f887d2ed112b16bd64f77b46e3227e
TaiFilipelli <taielfilipelli@gmail.com>
2025-04-14 09:56:13 -0300

    new filters component

---
+++ src/components/Filters.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FiltersComponent = () => {
+  return (
+    <nav className="flex flex-row">
+      <h1>Acá estarán los filtros</h1>
+    </nav>
+  )
+}
+
+export default FiltersComponent
