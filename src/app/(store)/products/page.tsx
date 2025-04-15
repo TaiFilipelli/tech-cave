f5a4e7a4fa2b821bea479d4307a1e84f1b5ca0fa
TaiFilipelli <taielfilipelli@gmail.com>
2025-04-15 11:43:32 -0300

    added flex direction XDDDD

--- src/app/(store)/products/page.tsx
+++ src/app/(store)/products/page.tsx
@@ -3,7 +3,7 @@ import ClientProductsList from './ClientProductList';
 
 export default function ProductsPage() {
   return (
-    <section className="flex items-center text-center p-10">
+    <section className="flex flex-col items-center text-center p-10">
       <h2 className="text-5xl font-bold mb-5">Productos</h2>
       <Suspense fallback={<div>Cargando filtros...</div>}>
         <ClientProductsList />
