edea57f9d9ffe6f3780f89b0adee8efa242f2373
TaiFilipelli <taielfilipelli@gmail.com>
2025-04-25 11:58:18 -0300

    minor responsive changes in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         <FontAwesomeIcon icon={faDragon} size="2xl"/>
         <h1 className="text-2xl font-bold"><span className="bg-gradient-to-r from-red-600 to-yellow-500 bg-clip-text text-transparent">Tech`s</span> Cave</h1>
       </section>
-      <section className="flex flex-row gap-5 items-center">
+      <section className="flex flex-col sm:flex-row gap-1 sm:gap-5 items-center">
         <Button as={Link} href="/products" className="bg-transparent text-black font-semibold text-lg">Productos</Button>
         <AuthButton/>
       </section>
