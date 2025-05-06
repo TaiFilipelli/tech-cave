48528eaeab62c699a5812180e9b60d03d061bf9d
TaiFilipelli <taielfilipelli@gmail.com>
2025-05-06 19:23:26 -0300

    changes in cart: now you cant buy unless you are logged in, new conditional rendering in modal.

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,13 +2,13 @@
 
 import { Drawer, DrawerHeader, DrawerBody, DrawerContent, DrawerFooter, Button, useDisclosure, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@heroui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping, faTrash, faFaceFrown, faMinusCircle, faPlusCircle  } from "@fortawesome/free-solid-svg-icons";
-import {faWhatsapp} from "@fortawesome/free-brands-svg-icons";
+import { faCartShopping, faTrash, faFaceFrown, faMinusCircle, faPlusCircle, faUserXmark,   } from "@fortawesome/free-solid-svg-icons";
+import {faWhatsapp, faGoogle} from "@fortawesome/free-brands-svg-icons";
 import React, { useEffect, useState } from "react";
 import { useCartStore } from "@/store/useCartStore";
 import { useRouter } from "next/navigation";
 // import api from "@/app/api/api";
-import { useSession } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 
 
 const Cart = () => {
@@ -16,6 +16,7 @@ const Cart = () => {
   const { cart, clearCart, removeFromCart, updateCartItem } = useCartStore((state) => state);
   const [products, setProducts] = useState(cart);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
   const {data:session} = useSession();
 
   const router = useRouter();
@@ -35,6 +36,13 @@ const Cart = () => {
       removeFromCart(id);
     }
   };
+
+  const handleBuy = () => {
+    if(session?.user && session?.accessToken){
+      setIsLogged(true);
+    }
+    setIsModalOpen(true);
+  }
 
   
   const sendMessage = () => {
@@ -88,19 +96,29 @@ const Cart = () => {
       <Modal isOpen={isModalOpen} onOpenChange={setIsModalOpen}>
       <ModalContent>
         <ModalHeader className="text-2xl font-bold">Finalizar compra</ModalHeader>
-        <ModalBody>
-          <h1>Seleccione el método de pago:</h1>
-          <ul className="gap-4 list-item">
-            <li className="mb-2">WhatsApp: Contacta directamente con el vendedor para transferencias o efectivo</li>
-            <li>MercadoPago: Paga con tarjeta de crédito, débito o con billeteras virtuales</li>
-          </ul>
-          <Button className="bg-green-500 text-white w-full font-semibold text-lg" startContent={<FontAwesomeIcon icon={faWhatsapp} size="xl"/>} onPress={sendMessage}>
-            WhatsApp
-          </Button>
-          <Button className="bg-blue-600 text-white w-full font-semibold text-lg" onPress={handleMP}>
-            MercadoPago
-          </Button>
-        </ModalBody>
+        {isLogged ?
+          <ModalBody>
+            <h1>Seleccione el método de pago:</h1>
+            <ul className="gap-4 list-item">
+              <li className="mb-2">WhatsApp: Contacta directamente con el vendedor para transferencias o efectivo</li>
+              <li>MercadoPago: Paga con tarjeta de crédito, débito o con billeteras virtuales</li>
+            </ul>
+            <Button className="bg-green-500 text-white w-full font-semibold text-lg" startContent={<FontAwesomeIcon icon={faWhatsapp} size="xl"/>} onPress={sendMessage}>
+              WhatsApp
+            </Button>
+            <Button className="bg-blue-600 text-white w-full font-semibold text-lg" onPress={handleMP}>
+              MercadoPago
+            </Button>
+          </ModalBody>
+        :
+          <ModalBody>
+            <FontAwesomeIcon icon={faUserXmark} size="2xl" className="animate-appearance-in"/>
+            <h1 className="text-2xl font-bold">Inicia sesión para continuar</h1>
+            <p className="text-gray-700 ">Para poder comprar, debes iniciar sesión.</p>
+            <Button onPress={() => signIn("google")} startContent={<FontAwesomeIcon icon={faGoogle} size="xl"/>} className="bg-blue-600 text-white w-full font-semibold text-lg p-6">Iniciar sesión</Button>
+          </ModalBody>
+        }
+
         <ModalFooter>
           <Button className="bg-red-600 w-full text-white font-semibold text-lg" onPress={() => setIsModalOpen(false)}>
             Cancelar
@@ -139,7 +157,7 @@ const Cart = () => {
           </DrawerBody>
           <DrawerFooter className="flex flex-col gap-2">
             <div className="flex flex-row justify-between gap-2">
-                <Button className="bg-green-500 text-white font-bold w-1/2" onPress={() => setIsModalOpen(true)}>Comprar</Button>
+                <Button className="bg-green-500 text-white font-bold w-1/2" onPress={handleBuy}>Comprar</Button>
                 <Button className="bg-red-600 text-white font-bold w-1/2" onPress={onClose}>Cerrar</Button>
             </div>
             <Button className="bg-gray-400 font-bold text-black" onPress={clearCart}><FontAwesomeIcon icon={faTrash}/>Limpiar carro</Button>
